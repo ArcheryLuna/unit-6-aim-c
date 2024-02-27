a6b20078b83069be66c0247de5f5894dc839838b
ArcheryLuna <76516425+ArcheryLuna@users.noreply.github.com>
2024-02-27 09:49:52 +0000

    added a quote

--- components/home-page/ceo-quote.tsx
+++ components/home-page/ceo-quote.tsx
@@ -1,9 +1,52 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQuoteLeft, faCalendarDay, faCoffeeBean } from '@fortawesome/pro-solid-svg-icons';
 
+import {
+    HoverCardContent,
+    HoverCardTrigger,
+    HoverCard
+} from "@/components/ui/hover-card";
+
+import {
+    Avatar,
+    AvatarImage,
+    AvatarFallback
+} from "@/components/ui/avatar";
 
 export default function CeoQuote() {
     return (
-        <>
-            
-        </>
+        <div className='my-6 lg:my-12'>
+            <FontAwesomeIcon icon={faQuoteLeft} className="text-4xl lg:text-6xl" />
+            <h2 className="text-2xl lg:text-4xl font-bold text-center lg:text-left">"
+                At Hot Beans, we're more than a team; we're a family that thrives on creativity and innovation. Our mission is to transform ideas into digital realities that make a difference. We're on the hunt for passionate developers ready to challenge the status quo and elevate their careers. Join us, and together, let's shape the future of technology.
+            "</h2>
+            <div className="flex items-center justify-center lg:justify-start mt-4">
+                <HoverCard>
+                    <HoverCardTrigger className='underline-hover'>
+                        <p className="text-base lg:text-lg text-right font-semibold">Felix Harper</p>
+                    </HoverCardTrigger>
+                    <HoverCardContent className='w-80 ml-4'>
+                        <div className="flex justify-between space-x-4">
+                        <Avatar>
+                            <AvatarImage src="/Employees/CEO.jpeg" />
+                            <AvatarFallback><FontAwesomeIcon icon={faCoffeeBean} /></AvatarFallback>
+                        </Avatar>
+                        <div className="space-y-1">
+                            <h4 className="text-sm font-semibold">Felix Harper</h4>
+                            <p className="text-sm">
+                            The Innovator and founder of the Hot Beans corperation.
+                            </p>
+                            <div className="flex items-center pt-2">
+                            <FontAwesomeIcon icon={faCalendarDay} className="mr-2 h-4 w-4 opacity-70" />{" "}
+                            <span className="text-xs text-muted-foreground">
+                                Founded May 2007
+                            </span>
+                            </div>
+                        </div>
+                        </div>
+                    </HoverCardContent>
+                </HoverCard>
+            </div>
+        </div>
     )
 }
