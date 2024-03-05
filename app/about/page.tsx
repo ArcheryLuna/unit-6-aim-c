96e3fcdeeda52b6eff73a04d24d2c0224aaff328
ArcheryLuna <76516425+ArcheryLuna@users.noreply.github.com>
2024-03-05 02:00:51 +0000

    Nighttime commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Metadata } from "next";
 import AboutParalaxLanding from "@/components/About-Us/Paralax-Landing";
+import AboutShortDesc from "@/components/About-Us/short-description";
 
 export const metadata: Metadata = {
   title: "Hot Beans | About Us",
@@ -12,8 +13,11 @@ export default function About() {
   return (
     <main className="dark:bg-[#191919] dark:bg-dot-[#dfe9e9]/[0.2] bg-dot-[#191919]/[0.2] relative">
       <AboutParalaxLanding />
-        <div className="relative z-10 px-6 py-6 lg:px-12 lg:py-12">
-          
+        <div className="relative z-10">
+          <AboutShortDesc />
+          <div className="px-6 py-6 lg:px-12 lg:py-12">
+
+          </div>
         </div>
       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-[#191919] bg-[#dfe9e9] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
     </main>
--- components/About-Us/short-description.tsx
+++ components/About-Us/short-description.tsx
@@ -1,9 +1,22 @@
 
 
-export default function AboutParalaxLanding() {
+export default function AboutShortDesc() {
     return (
-        <>
-        
-        </>    
+        <div className="mt-1 px-6 py-6 lg:px-12 lg:py-12 text-[#dfe9e9] bg-opacity-90 lg:mt-12 bg-[url('/unsplashed/person-programming-in-the-dark.jpg')]">
+            <div className="grid gap-4 grid-cols-1 lg:grid-cols-2">
+                <div>
+                    <h1 className="text-3xl lg:text-5xl font-black">A breif history</h1>
+                    <p className="text-xl text-gray-400">What we were, to who we are now</p>
+                </div>
+                <div>
+                    <p className="mb-12">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam libero, nemo aut officia distinctio eveniet sapiente quidem laboriosam adipisci esse quo delectus rerum voluptatibus reiciendis veritatis quasi similique? Tenetur, quia!
+                    </p>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil vel, in pariatur corporis quo reprehenderit assumenda voluptatibus beatae ipsam modi enim optio soluta voluptatem itaque libero nisi, quidem, ab veniam.
+                    </p>
+                </div>
+            </div>
+        </div>    
     )
 }
